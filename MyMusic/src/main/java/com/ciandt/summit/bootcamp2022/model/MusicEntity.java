219b4633d6ab8da5071e9ae3c05c5ffdb31ba550
Dara S. Mariano <darasouza@ciandt.com>
2022-07-26 10:18:31 -0300

    [DONE] - Music and Artist Entitis created

--- MyMusic/src/main/java/com/ciandt/summit/bootcamp2022/model/MusicEntity.java
+++ MyMusic/src/main/java/com/ciandt/summit/bootcamp2022/model/MusicEntity.java
@@ -1,6 +1,7 @@
 package com.ciandt.summit.bootcamp2022.model;
 
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -13,6 +14,7 @@ import java.util.UUID;
 @Getter
 @Setter
 @NoArgsConstructor
+@AllArgsConstructor
 public class MusicEntity {
 
     @Id
@@ -22,7 +24,7 @@ public class MusicEntity {
     @Column(name = "Nome")
     private String name;
 
-    @Column(name = "ArtistaId")
+    @JoinColumn(name = "ArtistaId")
     @ManyToOne(optional = false, fetch = FetchType.LAZY)
     private ArtistEntity artist;
 
